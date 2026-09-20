4cf3abe499ad93cc42e5879735eead6427abec3b
Ahmad Naji <ahmadnajee373@gmail.com>
2026-09-20 21:27:51 +0430

    working with pandas library

--- Day 25/main.py
+++ Day 25/main.py
@@ -1,4 +1,18 @@
+# import csv
+# from tokenize import Number
 
-with open("./Day 25/weather_data .csv") as data_file:
-    data = data_file.readlines()
-    print(data)
+# with open("./Day 25/weather_data .csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperature = []
+#     for row in data:
+#         if row[1] != 'temp':
+#             temperature.append(int(row[1]))
+        
+
+
+# print(temperature)
+
+import pandas
+
+data = pandas.read_csv("./Day 25/weather_data .csv")
+print(data)
